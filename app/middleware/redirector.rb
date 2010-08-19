3d37b4752219fa1853e2f9cf30de54bac2fa92f0
Bryan Helmkamp <bryan@brynary.com>
2010-08-19 13:35:05 -0400

    Don't compare port numbers in redirector

--- app/middleware/redirector.rb
+++ app/middleware/redirector.rb
@@ -6,7 +6,7 @@ class Redirector
   def call(env)
     request = Rack::Request.new(env)
 
-    if request.host != HOST && request.path !~ %r{^/api}
+    if request.host != expected_host_without_port && request.path !~ %r{^/api}
       fake_request = Rack::Request.new(env.merge("HTTP_HOST" => HOST))
       [301, {"Location" => fake_request.url}, []]
     elsif request.path =~ %r{^/(book|chapter|export|read|shelf|syndicate)}
@@ -15,4 +15,11 @@ class Redirector
       @app.call(env)
     end
   end
+
+private
+
+  def expected_host_without_port
+    HOST.to_s.gsub(/:\d+\z/, '')
+  end
+
 end
